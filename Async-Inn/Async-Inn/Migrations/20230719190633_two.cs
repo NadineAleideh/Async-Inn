0c990399dc9878a7be2138b43df8275cb6a5124d
Nadine Aleideh <nadinealeideh75@gmail.com>
2023-07-19 22:39:24 +0300

    Lab12 done

---
+++ Async-Inn/Async-Inn/Migrations/20230719190633_two.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Async_Inn.Migrations
+{
+    /// <inheritdoc />
+    public partial class two : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Coffee Maker" },
+                    { 2, "AC" },
+                    { 3, "Sea View" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Country", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Aqaba", "Jordan", "AsyncInn1", "0096232034020", "Aqaba", "Al-Hussein St" },
+                    { 2, "Petra", "Jordan", "AsyncInn2", "0096232157201", "Petra", "Queen Rania St" },
+                    { 3, "Amman", "Jordan", "AsyncInn3", "0096265528822", "Amman", "Madina Monawarah St" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, "Studio" },
+                    { 2, 1, "Single Room" },
+                    { 3, 2, "Double Room" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
